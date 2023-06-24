7087dc0ddd476758dd389ba7d870d72e5d310f70
MariaZinevych <123274407+MariaZinevych@users.noreply.github.com>
2023-06-24 23:00:51 +0200

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const bthStart = document.querySelector('button[data-start]');
+const bthStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
 
+let timerId = null;
+
+bthStart.addEventListener('click', () => {
+  (timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000)),
+    bthStart.setAttribute('disabled', ''),
+    bthStop.removeAttribute('disabled');
+});
+
+bthStop.addEventListener('click', () => {
+  clearInterval(timerId);
+  bthStop.setAttribute('disabled', ''), bthStart.removeAttribute('disabled');
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
