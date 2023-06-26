73effa9eb235fab6d738ed4e43aa36fd448c9e3d
MariaZinevych <123274407+MariaZinevych@users.noreply.github.com>
2023-06-26 09:17:39 +0000

    Deploying to gh-pages from @ MariaZinevych/goit-js-hw-09@985a90eaadc1911ff3d776a29df8d414f97edf79 🚀

---
+++ 03-promises.82218207.js
@@ -0,0 +1,2 @@
+document.querySelector(".form"),document.querySelector('button[type="submit"]'),document.querySelector(["delay"]),document.querySelector(["step"]),document.querySelector(["amount"]);
+//# sourceMappingURL=03-promises.82218207.js.map
